fd4f85ac7549cbebab18ee8c1327d4ac536e5872
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2021-11-13 15:39:09 +0500

    add the user/register service/controller/repo/sqlite integration/api etc

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+
+import { CreateUserDto } from './dtos/create-user.dtos';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log('createUser body is: ', body);
+    this.usersService.create(body.email, body.password);
+  }
+}
